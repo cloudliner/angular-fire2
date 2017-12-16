fbdaf400c2f18c7e9b1e9297ddfc3b7d270b6463
cloudliner <cloudliner.jp@gmail.com>
2017-12-16 12:48:37 +0900

    Add Firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDjPWBnzVdGuFbJVmEZUOdYgtlTJqsXnDs",
+    authDomain: "angular-fire2-e799e.firebaseapp.com",
+    databaseURL: "https://angular-fire2-e799e.firebaseio.com",
+    projectId: "angular-fire2",
+    storageBucket: "angular-fire2.appspot.com",
+    messagingSenderId: "397017612987"
+  }
 };
