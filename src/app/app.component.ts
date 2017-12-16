0257b2351415ea7b04e930c7adad61c1abfc6e0b
cloudliner <cloudliner.jp@gmail.com>
2017-12-16 16:01:21 +0900

    Retrieving nested collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,32 @@ interface Item {
   name:string
 }
 
+interface Task {
+  limit:Date
+}
+
 @Component({
   selector: 'app-root',
   template: `
     <div>
       {{ (item | async)?.name }}
     </div>
+    <div>
+      <li class="text" *ngFor="let task of tasks | async">
+        {{ task.limit }}
+      </li>
+    </div>
   `
 })
 export class AppComponent {
   itemDoc: AngularFirestoreDocument<Item>;
   item: Observable<Item>;
+  tasks: Observable<any>;
+
   constructor(private afs: AngularFirestore) {
-    this.itemDoc = afs.doc<Item>('items/uVBxzJMfc9qdm64rJFsC');
+    this.itemDoc = afs.doc<Item>('items/1');
     this.item = this.itemDoc.valueChanges();
+    this.tasks = this.itemDoc.collection<Task>('tasks').valueChanges();
   }
   update(item: Item) {
     this.itemDoc.update(item);
