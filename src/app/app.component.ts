df03644a0253c1e4faceb12a7684481a814460cf
cloudliner <cloudliner.jp@gmail.com>
2017-12-17 18:08:52 +0900

    Dynamic querying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,72 @@
 import { Component, Input } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/combineLatest';
 
 export interface Item {
-  name: string;
-  test: string;
+  text: string;
+  color: string;
+  size: string;
 }
 
 @Component({
   selector: 'app-root',
   template: `
+  <div *ngIf="items$ | async; let items; else loading">
     <ul>
-      <li *ngFor="let item of items | async">
-        <span>{{ item.id }}</span> : 
-        <span>{{ item.name }}</span> : 
-        <span>{{ item.test }}</span>
-        <button type="button" (click)="removeItem(item)">Remove</button>
+      <li *ngFor="let item of items">
+        {{ items.text }}
       </li>
     </ul>
-    <div>
-      <label>name: </label>
-      <input type="text" [(ngModel)]="newItem.name">
-      <label>test: </label>
-      <input type="text" [(ngModel)]="newItem.test">
-      <button type="button" (click)="addItem(newItem)">Add</button>
-    </div>
+    <div *ngIf="items.length === 0">No results, try clearing filters</div>
+  </div>
+  <ng-template #loading>Loading&hellip;</ng-template>
+  <div>
+    <h4>Filter by size</h4>
+    <button (click)="filterBySize('small')">Small</button>
+    <button (click)="filterBySize('medium')">Medium</button>
+    <button (click)="filterBySize('large')">Large</button>
+    <button (click)="filterBySize(null)" *ngIf="this.sizeFilter$.getValue()">
+      <em>clear filter</em>
+    </button>
+  </div>
+  <div>
+    <h4>Filter by color</h4>
+    <button (click)="filterByColor('red')">Red</button>
+    <button (click)="filterByColor('green')">Green</button>
+    <button (click)="filterByColor('blue')">Blue</button>
+    <button (click)="filterByColor(null)" *ngIf="this.colorFilter$.getValue()">
+      <em>clear color</em>
+    </button>
+  </div>
   `
 })
-
 export class AppComponent {
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
-  newItem: Item = {
-    name: '名称未設定',
-    test: 'テスト',
-  };
-  constructor(private afs: AngularFirestore) {
-    this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges(['added', 'removed']);
+  items$: Observable<Item[]>;
+  sizeFilter$: BehaviorSubject<string|null>;
+  colorFilter$: BehaviorSubject<string|null>;
+
+  constructor(afs: AngularFirestore) {
+    this.sizeFilter$ = new BehaviorSubject(null);
+    this.colorFilter$ = new BehaviorSubject(null);
+    this.items$ = Observable.combineLatest(
+      this.sizeFilter$,
+      this.colorFilter$
+    ).switchMap(([size, color]) =>
+      afs.collection('items', ref => {
+        let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+        if (size) { query = query.where('size', '==', size) };
+        if (color) { query = query.where('color', '==', color) };
+        return query;
+      }).valueChanges()
+    );
   }
-  addItem(item) {
-    this.itemsCollection.add(item);
+  filterBySize(size: string|null) {
+    this.sizeFilter$.next(size);
   }
-  removeItem(item) {
-    // どうやるの？
+  filterByColor(color: string|null) {
+    this.colorFilter$.next(color);
   }
 }
