879b67f57106f2bf8b0e7c3d14bc698bd791b90b
cloudliner <cloudliner.jp@gmail.com>
2017-12-17 15:17:17 +0900

    Limiting events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,38 +2,47 @@ import { Component, Input } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
-export interface AccountLogItem {
-  description: string;
-  amount: number;
-}
-
-export interface AccountLogItemId extends AccountLogItem {
-  id: string;
+export interface Item {
+  name: string;
+  test: string;
 }
 
 @Component({
   selector: 'app-root',
   template: `
     <ul>
-      <li *ngFor="let log of accountLogs | async">
-        {{ log.description }} for {{ log.amount }}
+      <li *ngFor="let item of items | async">
+        <span>{{ item.id }}</span> : 
+        <span>{{ item.name }}</span> : 
+        <span>{{ item.test }}</span>
+        <button type="button" (click)="removeItem(item)">Remove</button>
       </li>
     </ul>
+    <div>
+      <label>name: </label>
+      <input type="text" [(ngModel)]="newItem.name">
+      <label>test: </label>
+      <input type="text" [(ngModel)]="newItem.test">
+      <button type="button" (click)="addItem(newItem)">Add</button>
+    </div>
   `
 })
 
 export class AppComponent {
-  private accountLogCollection: AngularFirestoreCollection<AccountLogItem>;
-  accountLogs: Observable<AccountLogItemId[]>;
-  constructor(private readonly afs: AngularFirestore) {
-    this.accountLogCollection = afs.collection<AccountLogItem>('accountLog');
-    this.accountLogs = this.accountLogCollection.auditTrail()
-      .map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as AccountLogItem;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        });
-      });
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  newItem: Item = {
+    name: '名称未設定',
+    test: 'テスト',
+  };
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('items');
+    this.items = this.itemsCollection.valueChanges(['added', 'removed']);
+  }
+  addItem(item) {
+    this.itemsCollection.add(item);
+  }
+  removeItem(item) {
+    // どうやるの？
   }
 }
